4447ae73abd7ae82d7af97fb72c0f7a8fd30083e
KevinAngeles <kevin.angeles@outlook.com>
2016-08-08 21:12:39 -0500

    js code linked to css and html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,3 @@
-//display msg html 'Press any key to get started'
-//below, display wins
 /*
 	function removeSpaces(str)
 	====================================
@@ -17,64 +15,83 @@ function removeSpaces(str) {
 	return str.replace(/\s+/g,'');
 }
 
+var audio = document.getElementById("myAudio");
+
 var game = {
 	charsGuessed: [],
-	hasStarted: false,
-	opportunities:15,
-	allowedCharacters: /^[a-zA-Z0-9]+$/,
+	currentWord: "",
+	currentAudioSource: "",
+	opportunities: 15,
+	allowedCharacters: /^[a-zA-Z0-9]+$/,//regex with the allowed characters
 	selectedSong: {},
-	songs: [{name:'Mighty Long Fall',singer:'One Ok Rock'},{name:'Low',singer:'floRida'},{name:'Talk Dirty',singer:'Jason Derulo'}],
+	songs: [{name:"Mighty Long Fall",singer:"One Ok Rock",country:"Japan",pic:"assets/images/artists/oneokrock.jpeg",bgpic:"assets/images/bgs/oneokrock_bg.jpg",musicSrc1:"assets/songs/mightylongfall.mp3",musicSrc2:"assets/songs/mightylongfall.ogg",picClass:"whiteBorder",textClass:"whiteFont"},{name:"Bring me to Life",singer:"Evanescence",country:"USA",pic:"assets/images/artists/evanescence.jpeg",bgpic:"assets/images/bgs/evanescence_bg.jpg",musicSrc1:"assets/songs/bringmetolife.mp3",musicSrc2:"assets/songs/bringmetolife.ogg",picClass:"whiteBorder",textClass:"whiteFont"},{name:"Low",singer:"floRida",country:"USA",pic:"assets/images/artists/florida.jpg",bgpic:"assets/images/bgs/florida_bg.jpg",musicSrc1:"assets/songs/low.mp3",musicSrc2:"assets/songs/low.ogg",picClass:"fireBorder",textClass:"fireFont"},{name:"Talk Dirty",singer:"Jason Derulo",country:"USA",pic:"assets/images/artists/jasonderulo.jpeg",bgpic:"assets/images/bgs/jasonderulo_bg.jpg",musicSrc1:"assets/songs/talkdirty.mp3",musicSrc2:"assets/songs/talkdirty.ogg",picClass:"whiteBorder",textClass:"whiteFont"}],
 	wins: 0,
 	getRandomSong: function(){
+		//This will get a random song from the array of songs and set it to selectedSong
 		this.selectedSong = this.songs[Math.floor(Math.random()*this.songs.length)];
 	},
 	initGame: function(){
-		game.getRandomSong();
-		game.hasStarted = true;
+		//This will reset some variables in order to begin the game
 		game.charsGuessed = [];
-		game.opportunities = 15;
+		game.getRandomSong();
+		this.setInitialCurrentWord();
 	},
+	setInitialCurrentWord: function(){
+		var txt = "";
+		//This will replace all the allowed characters of the song name with an underscore ('_') 
+		//in order to create a song name that can be guessed by the user
+		this.selectedSong.name.split('').forEach(function(c) {
+   			if( !game.allowedCharacters.test(c) )
+	   			txt=txt+c;
+   			else
+   				txt=txt+"_";
+
+		});
+   		this.currentWord = txt;
+	}
 }
 
+/* Setting initial parameters for a new game */
+game.initGame();
+/* Check if browser supports mp3
+	===========================
+	Required when using multiple sources in Chrome, otherwise it won't work
+	because there is a bug in Chrome with the source tags in html.
+*/
+if( audio.canPlayType('audio/mpeg;') )
+	game.currentAudioSource = game.selectedSong.musicSrc1;
+else
+	game.currentAudioSource = game.selectedSong.musicSrc2;
+
+document.getElementById("currentWord").innerHTML = game.currentWord;
+document.getElementById("charactersGuessed").innerHTML = "[ ]";
+
+/* Listen for a keyup event */
 document.onkeyup = function(event) {
 	//detect which key the user pressed
 	var userKey = String.fromCharCode(event.keyCode).toLowerCase();
 	//if the user only press a letter or a number
 	if( game.allowedCharacters.test(userKey) )
 	{
-		var currentWorld = "";
-		//if is a new game
-		if( !game.hasStarted )
+		//if the user hadn't guessed the character before (it is not in array charsGuessed)
+		if( game.charsGuessed.indexOf(userKey) < 0 )
 		{
-			//select a random object song from the list
-			game.initGame();
-		}
-
-		//if user had already guessed the character
-		if( game.charsGuessed.indexOf(userKey) > -1 )
-		{
-			//do nothing
-			console.log('had already guessed');
-		}
-		//if the user hadn't guessed the character before (it is not in charsGuessed)
-		else
-		{
+			//reset currentWord
+			game.currentWord="";
 			//if the user discovered a new character that is part of the selectedSong name
 			if( game.selectedSong.name.toLowerCase().indexOf(userKey) > -1 )
 			{
 				for(var i = 0; i < game.selectedSong.name.length; i++)
 				{
-					if( userKey === game.selectedSong.name[i].toLowerCase() || game.selectedSong.name[i] === ' ' || game.charsGuessed.indexOf(game.selectedSong.name[i].toLowerCase()) > -1 )
+					if( userKey === game.selectedSong.name[i].toLowerCase() || !game.allowedCharacters.test(game.selectedSong.name[i])  || game.charsGuessed.indexOf(game.selectedSong.name[i].toLowerCase()) > -1 )
 					{
-						currentWorld = currentWorld+game.selectedSong.name[i];
+						game.currentWord = game.currentWord + game.selectedSong.name[i];
 					}
 					else
 					{
-						currentWorld = currentWorld+"_";
+						game.currentWord = game.currentWord + "_";
 					}
 
-					if( i < (game.selectedSong.name.length - 1) )
-						currentWorld = currentWorld + " ";
 				}
 				
 				console.log('new');
@@ -84,19 +101,17 @@ document.onkeyup = function(event) {
 			{
 				for( var i = 0; i < game.selectedSong.name.length; i++ )
 				{
-					//if empty character
+					//if is not a number or letter
 					//or character had already been guessed and it is part of the selected song name  
-					if( (game.selectedSong.name[i] === ' ') || (game.charsGuessed.indexOf(game.selectedSong.name[i].toLowerCase()) > -1) )
+					if( !game.allowedCharacters.test(game.selectedSong.name[i]) || (game.charsGuessed.indexOf(game.selectedSong.name[i].toLowerCase()) > -1) )
 					{
-						currentWorld = currentWorld + game.selectedSong.name[i];
+						game.currentWord = game.currentWord + game.selectedSong.name[i];
 					}
 					else
 					{
-						currentWorld = currentWorld + "_";
+						game.currentWord = game.currentWord + "_";
 					}
 
-					if( i < (game.selectedSong.name.length - 1) )
-						currentWorld = currentWorld + " ";
 				}	
 				//decrease oportunities
 				game.opportunities = game.opportunities - 1;
@@ -104,26 +119,61 @@ document.onkeyup = function(event) {
 				//if you dont have more opportunities
 				if( game.opportunities <= 0 )
 				{
-					console.log("You Lost!");
-					console.log("The song was " + game.selectedSong.name);
-					//change the state of the game
-					game.hasStarted = false;
+					game.initGame();
+					//update the source in order to be used next time (object game)
+					if (audio.canPlayType('audio/mpeg;'))
+						game.currentAudioSource = game.selectedSong.musicSrc1;
+					else
+						game.currentAudioSource = game.selectedSong.musicSrc2;
+					//show message
+					document.getElementById("songTitle").innerHTML = "You Lost!";
+					//reset opportunities
+					game.opportunities=15;
+					//reset wins
+					game.wins = 0;
+					document.getElementById("winsQty").innerHTML = game.wins;
+
 				}
 			}
+			//add the new character to the array of characters already guessed
 			game.charsGuessed.push(userKey);
 			//if you finally guessed the complete song name
-			if( removeSpaces(currentWorld.toLowerCase()) === removeSpaces(game.selectedSong.name.toLowerCase()) )
+			if( removeSpaces(game.currentWord.toLowerCase()) === removeSpaces(game.selectedSong.name.toLowerCase()) )
 			{
 				console.log("Congratulations, you win!");
-				game.win = game.win + 1;
-				//change the state of the game
-				game.hasStarted = false;
+				game.wins = game.wins + 1;
+				document.getElementById("winsQty").innerHTML = game.wins;
+				document.getElementById("songTitle").innerHTML = game.selectedSong.name;
+				//change pic (html5)
+				document.getElementById("artistPic").setAttribute("src", game.selectedSong.pic);
+				//change pic css (html5) border
+				document.getElementById("artistPic").className = game.selectedSong.picClass;
+				//change secondContainer Css (html5) border
+				document.getElementById("secondContainer").className = "col-xs-8 col-sm-8 col-md-8 col-lg-8 col-xs-offset-2 col-sm-offset-2 col-md-offset-2 col-lg-offset-2 "+game.selectedSong.picClass;
+				//set the current background (html5)
+				document.getElementById("bg").style.backgroundImage='url("'+game.selectedSong.bgpic+'")';
+				//change text css (html5)
+				document.getElementById("titleContainer").className = "row "+game.selectedSong.textClass;
+				document.getElementById("centralDiv").className = "col-xs-6 col-sm-6 col-md-6 col-lg-6 "+game.selectedSong.textClass;
+				document.getElementById("songTitle").className = "row "+game.selectedSong.textClass;
+				//stop the music if there was a song playing (html5)
+				audio.src="";
+				//get the current source of the song (html5)
+				audio.src=game.currentAudioSource;
+				//play the music (html5)
+				audio.play();
+				//start game
+				game.initGame();
+				//update the source in order to be used next time (object game)
+				if (audio.canPlayType('audio/mpeg;'))
+					game.currentAudioSource = game.selectedSong.musicSrc1;
+				else
+					game.currentAudioSource = game.selectedSong.musicSrc2;
 			}
 		}
 
-		console.log(currentWorld);
-		console.log("Letters Already Guessed " + game.charsGuessed.toString());
-		console.log("Number of Guesses Remaining " + game.opportunities);
-		console.log(game);
+		document.getElementById("currentWord").innerHTML = game.currentWord;
+		document.getElementById("charactersGuessed").innerHTML = "["+game.charsGuessed.toString()+"]";
+		document.getElementById("guessesRemaining").innerHTML = game.opportunities;
 	}
 }
